5b12ab7f2b46b0b3de9e33ed86d3a5beba824c79
otajisan <taji.taji.otajisan@gmail.com>
2016-03-22 16:58:54 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='WebScraper',
+        version='0.0.1',
+        description='Python Web Scraper',
+        author='otajisan',
+        package_dir = {'lib': 'lib'}
+    )
